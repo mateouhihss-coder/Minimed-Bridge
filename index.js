5449508ffea9be4b4b030d1c4b71097de18e2c6e
mateouhihss-coder <mateouhihss@gmail.com>
2026-07-12 18:03:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('minimed-connect-to-nightscout');
+try {
+  console.log("Инициализация загрузчика Medtronic...");
+  require('minimed-connect-to-nightscout');
+  console.log("Загрузчик успешно запущен, ожидаем подключение...");
+} catch (error) {
+  console.error("КРИТИЧЕСКАЯ ОШИБКА ПРИ ЗАПУСКЕ:");
+  console.error(error.message);
+  console.error(error.stack);
+  process.exit(1);
+}
+
